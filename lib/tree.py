34b1e2fd9728c49a254f8818015b5d7a9d181c09
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-08-21 10:25:34 +0300

    Using Depth First Search recursively to find the node whose id matches the given id

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,19 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self._dfs(self.root, id)
+  
+  def _dfs(self, node, target_id):
+    if not node:
+      return None
+    if node["id"] == target_id:
+      return node
+    for child in node['children']:
+      result = self._dfs(child, target_id)
+      if result:
+        return result
+    return None
+
+
+
+  
